e3b1158e9ab674dcc8fb16a77071dc1300681d6c
Tomas Dermisek <tomas.dermisek@optus.com.au>
2020-07-10 11:23:11 +1000

    Replaced discontinued 'request` library with with 'got'

--- test/integration/api.spec.js
+++ test/integration/api.spec.js
@@ -1,6 +1,6 @@
 import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import request from 'request-promise-native';
+import got from 'got';
 import { SSM, SharedIniFileCredentials } from 'aws-sdk';
 
 import Constants from '../../src/configs/constants';
@@ -9,11 +9,9 @@ import ParameterStoreService from '../../src/services/parameter-store-service';
 chai.use(chaiAsPromised);
 
 describe('API integration', () => {
-  /**
-   * Un-skip this step if you wish to use parameter store values, otherwise configure you .env
-   */
-  before.skip(async () => {
-    const { AWS_STAGE = 'dev', AWS_PROFILE = 'dev', AWS_REGION = 'ap-southeast-2' } = process.env;
+
+  before(async () => {
+    const { AWS_STAGE = 'dev', AWS_PROFILE = 'default', AWS_REGION = 'ap-southeast-2' } = process.env;
 
     // Setup parameter store paths
     process.env.SSM_API_KEY = `/${AWS_STAGE}/api/api_key`;
@@ -21,7 +19,7 @@ describe('API integration', () => {
 
     // Load parameter store values
     await new ParameterStoreService({
-      parameters: Constants.API_PARAMETERS,
+      parameters: Constants.parameters,
       useCache: false,
       ssm: new SSM({
         region: AWS_REGION,
@@ -32,24 +30,24 @@ describe('API integration', () => {
 
   it('Unauthorized requests should return 403 response', () =>
     expect(
-      request({
-        url: `${process.env[Constants.API_URL.name]}/123?message=abc`,
-        json: true,
+      got({
+        url: `${process.env[Constants.apiUrl.name]}/123?message=abc`,
+        responseType: 'json',
       }),
     ).to.be.rejectedWith('Forbidden'));
 
-  it('Valid requests should return correct response', () =>
-    expect(
-      request({
-        url: `${process.env[Constants.API_URL.name]}/123?message=abc`,
-        headers: { 'x-api-key': process.env[Constants.API_KEY.name] },
-        json: true,
-      }),
-    ).to.eventually.eql({
+  it('Valid requests should return correct response', async () => {
+    const { body } = await got({
+      url: `${process.env[Constants.apiUrl.name]}/123?message=abc`,
+      headers: { 'x-api-key': process.env[Constants.apiKey.name] },
+      responseType: 'json',
+    })
+    expect(body).to.eql({
       data: {
         id: '123',
         type: 'response',
         attributes: { message: 'abc' },
       },
-    }));
+    })
+  });
 });
